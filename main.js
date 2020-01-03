dbbb470502a08a9c26968193d67e520f410d8a57
michmay <michael.maygothling@gmail.com>
2020-01-04 01:41:02 +1100

    refactored posts

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var url = "http://192.168.0.2/api/ZBmlmu00Kpy23xBCjtLQCCinBYJaqjeULVNEWAmB/lights/1/state"
 const roomName = document.getElementById("room-name")
 
-
 const switchURL = () => {
     var switcher = document.getElementById("room-switch").checked
 
@@ -17,64 +16,52 @@ const switchURL = () => {
     }
 }
 
+function sendPut(url, payload) {
 
-function turnLightOff() {
-    
     var params = {
         headers:{
             "content-type":"application/json"
         },
-        body: JSON.stringify({on:false}),
+        body: JSON.stringify(payload),
         method:"PUT"}
     fetch(url, params)
     .then(data=>{console.log(data)})
     .catch(error=>console.log(error))
+
+
+}
+
+function turnLightOff() {
+    
+    var payload = {on:false};
+    sendPut(url, payload);
 }
 
 function turnLightOn() {
-    var params = {
-        headers:{
-            "content-type":"application/json"
-        },
-        body: JSON.stringify({
-            on:true,
-            bri:255
-        }),
-        method:"PUT"}
-    fetch(url, params)
-    .then(data=>{console.log("haha")})
-    .catch(error=>console.log(error))
+
+    var payload = {
+        on:true,
+        bri:255
+    };
+    sendPut(url, payload);
 }
 
 function dimLight() {
-    var params = {
-        headers:{
-            "content-type":"application/json"
-        },
-        body: JSON.stringify({
-            on:true,
-            bri:100
-        }),
-        method:"PUT"}
-    fetch(url, params)
-    .then(data=>{console.log("haha")})
-    .catch(error=>console.log(error))
+    var payload = {
+        on:true,
+        bri:100
+    };
+    sendPut(url, payload);
+
 }
 
 function setBrightness(brightness) {
     var brightness = parseInt(brightness)
-    var params = {
-        headers:{
-            "content-type":"application/json"
-        },
-        body: JSON.stringify({
-            on:true,
-            bri:brightness
-        }),
-        method:"PUT"}
-    fetch(url, params)
-    .then(data=>{console.log("haha")})
-    .catch(error=>console.log(error))
+    var payload = {
+        on:true,
+        bri:brightness
+    };
+    sendPut(url, payload);
 
     var opacity = Math.max(Math.floor(brightness/255*100), 10)
     console.log("opacity is " + opacity)
