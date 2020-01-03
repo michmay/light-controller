ae68d7981da193e1a29f6b743eeaafafdfff4a40
michmay <michael.maygothling@gmail.com>
2020-01-03 21:05:56 +1100

    pushing take-image

--- take_image.js
+++ take_image.js
@@ -1,7 +1,15 @@
+/*
+
 var dirname = '\\c:\\Program Files\\wkhtmltopdf'
 var wkhtmltoimage = require('wkhtmltoimage').setCommand(dirname + '\\bin\\wkhtmltoimage');
 
 wkhtmltoimage.generate('index.html', { output: 'current-state.jpeg' });
-
+*/
 
 var fs = require=('fs');
+
+fs.readFile('README.md', 'utf8', function(err, contents) {
+    console.log(contents);
+});
+ 
+console.log('after calling readFile');
